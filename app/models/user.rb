5db65571fa31b1bb21a00f2433ff0553ea1fde83
Chad Montoya <chadmontoya21@yahoo.com>
2018-02-23 04:41:55 +0000

    automatically backed up by learn

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,14 +1,4 @@
 class User < ActiveRecord::Base
   has_secure_password
   validates_presence_of :username
-
-  def change_balance(params)
-    if !!params[:deposite]
-      amount = params[:deposite].to_i
-      total = user.balance.to_i + amount
-    else
-      amount = params[:withdraw].to_i
-      total = user.balance.to_i - amount
-    end
-  end
 end
